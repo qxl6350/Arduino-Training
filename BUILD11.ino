06e2173d1d320e1372c05beea6c8be05be3e4660
qxl6350 <qxl6350@g.coppellisd.com>
2016-12-12 09:43:03 -0600

    Add files via upload

---
+++ BUILD11.ino
@@ -0,0 +1,50 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+  The circuit:
+ * LED connected from digital pin 13 to ground.
+  * Note: On most Arduino boards, there is already an LED on the board
+ connected to pin 13, so you don't need any extra components for this example.
+  
+ Created 1 June 2005
+ By David Cuartielles
+ http://arduino.cc/en/Tutorial/Blink
+ based on an orginal by H. Barragan for the Wiring i/o board
+ */
+ #include <Servo.h> 
+
+Servo myservo;   // Relay connected to digital pin 2   <-----Change this to pin 2
+int pos = 0;
+
+int ledPin =  2;    // Relay connected to digital pin 2   <-----Change this to pin 2
+
+// The setup() method runs once, when the sketch starts
+
+void setup()   {                
+  // initialize the digital pin as an output:
+  pinMode(ledPin, OUTPUT);     
+    myservo.attach(9); 
+    
+}
+
+// the loop() method runs over and over again,
+// as long as the Arduino has power
+
+void loop()                     
+{
+  digitalWrite(ledPin, HIGH);   // set the LED on
+  delay(1000);                  // wait for a second
+  digitalWrite(ledPin, LOW);    // set the LED off
+  delay(1000);                  // wait for a second
+    for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+}
+r
---
+++ BUILD12.ino
@@ -0,0 +1,102 @@
+/*
+  RGB_LED_Color_Fade_Cycle.pde
+  
+  Cycles through the colors of a RGB LED
+  Written for SparkFun Arduino Inventor's Kit CIRC-RGB
+*/
+
+
+// LED leads connected to PWM pins
+const int RED_LED_PIN = 9;
+const int GREEN_LED_PIN = 10;
+const int BLUE_LED_PIN = 11;
+
+
+int ledDigitalOne[] = {9, 10, 11};
+
+
+int sensorPin = A0;
+
+const int DISPLAY_TIME = 100;
+
+// Used to store the current intensity level of the individual LEDs
+int redIntensity = 0;
+int greenIntensity = 0;
+int blueIntensity = 0;
+
+int setColor = (redIntensity, greenIntensity, blueIntensity);
+
+int sensorValue = 0;
+
+// Length of time we spend showing each color
+
+
+void setup() {
+  // No setup required.
+    for(int i = 0; i < 3; i++){
+   pinMode(ledDigitalOne[i], OUTPUT);   //Set the three LED pins as outputs
+  }
+  Serial.begin(9600);
+}
+
+void loop() {
+    sensorValue = analogRead(sensorPin);
+    Serial.println(sensorValue);
+    /*
+  // Cycle color from red through to green
+  // (In this loop we move from 100% red, 0% green to 0% red, 100% green)
+  for (greenIntensity = 0; greenIntensity <= 255; greenIntensity+=5) {
+        redIntensity = 255-greenIntensity;
+        analogWrite(GREEN_LED_PIN, greenIntensity);
+        analogWrite(RED_LED_PIN, redIntensity);
+        delay(DISPLAY_TIME);
+  }
+  // Cycle color from green through to blue
+  // (In this loop we move from 100% green, 0% blue to 0% green, 100% blue)  
+  for (blueIntensity = 0; blueIntensity <= 255; blueIntensity+=5) {
+        greenIntensity = 255-blueIntensity;
+        analogWrite(BLUE_LED_PIN, blueIntensity);
+        analogWrite(GREEN_LED_PIN, greenIntensity);
+        delay(DISPLAY_TIME);
+  }
+  // Cycle cycle from blue through to red
+  // (In this loop we move from 100% blue, 0% red to 0% blue, 100% red)    
+  for (redIntensity = 0; redIntensity <= 255; redIntensity+=5) {
+        blueIntensity = 255-redIntensity;
+        analogWrite(RED_LED_PIN, redIntensity);
+        analogWrite(BLUE_LED_PIN, blueIntensity);
+        delay(DISPLAY_TIME);
+  }
+  */
+  
+
+
+  if(sensorValue <= 255) {
+    redIntensity = 255; 
+    greenIntensity = sensorValue;
+    blueIntensity= 0;
+        analogWrite(GREEN_LED_PIN, greenIntensity);
+        analogWrite(RED_LED_PIN, redIntensity);
+        delay(DISPLAY_TIME);
+  }
+  if(255 <= sensorValue <= 510) {
+    redIntensity = 255-(sensorValue-255); 
+    greenIntensity = 255;
+    blueIntensity= sensorValue-255;
+        analogWrite(BLUE_LED_PIN, blueIntensity);
+        analogWrite(GREEN_LED_PIN, greenIntensity);
+        delay(DISPLAY_TIME);
+  }
+  if(510 <= sensorValue <= 765) {
+    redIntensity = 0; 
+    greenIntensity = 255-(sensorValue-510);
+    blueIntensity= sensorValue-510;
+        analogWrite(RED_LED_PIN, redIntensity);
+        analogWrite(BLUE_LED_PIN, blueIntensity);
+        delay(DISPLAY_TIME);
+  }
+
+
+  
+}
+
